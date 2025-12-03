a12bafd94d7b6ba2aada5424023c0c95b5e33ee2
Michael <spinnetc@student.21-school.ru>
2025-12-03 19:58:30 +0300

    initialize Spring Boot project

---
+++ rosinka-backend/src/main/java/com/rosinka/backend/RosinkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rosinka.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosinkaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RosinkaBackendApplication.class, args);
+    }
+
+}
---
+++ rosinka-backend/src/test/java/com/rosinka/backend/RosinkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosinka.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosinkaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
